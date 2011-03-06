47353d28b83e0e8b12d1ef0a3c3aa121268cd775
Ilmari Heikkinen <ilmari@banigold.(none)>
2011-03-06 19:38:38 +0200

    src/FLARParam.js: go back to a less hacky FLARParam.js

--- src/FLARParam.js
+++ src/FLARParam.js
@@ -37,11 +37,14 @@ FLARParam = ASKlass('FLARParam', NyARParam,
 {
   FLARParam : function(w,h)
   {
-    this._screen_size.w = w || 640;
-    this._screen_size.h = h || 480;
-    var dist = new FloatVector([w/2, h/2, 26.2, 1.1127565206658486]);
-    var projection = new FloatVector([900.9514702992245, 0, w/2-0.5, 0,
-                                      0, 860.0941816535367, h/2-0.5, 0,
+    w = w || 640;
+    h = h || 480;
+    this._screen_size.w = w;
+    this._screen_size.h = h;
+    var f = (w/h) / (4/3);
+    var dist = new FloatVector([w/2, 1.1*h/2, 26.2, 1.0127565206658486]);
+    var projection = new FloatVector([f*700.9514702992245, 0, w/2, 0,
+                                      0, 726.0941816535367, h/2, 0,
                                       0, 0,                 1,     0]);
     this.setValue(dist, projection);
   }
