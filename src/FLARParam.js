4f27678cdd98a471e1b21250be40ee35863e089f
Ilmari Heikkinen <ilmari@banigold.(none)>
2011-03-06 19:44:31 +0200

    src/FLARParam.js: bring back the half-pixel tweaks

--- src/FLARParam.js
+++ src/FLARParam.js
@@ -43,8 +43,8 @@ FLARParam = ASKlass('FLARParam', NyARParam,
     this._screen_size.h = h;
     var f = (w/h) / (4/3);
     var dist = new FloatVector([w/2, 1.1*h/2, 26.2, 1.0127565206658486]);
-    var projection = new FloatVector([f*700.9514702992245, 0, w/2, 0,
-                                      0, 726.0941816535367, h/2, 0,
+    var projection = new FloatVector([f*700.9514702992245, 0, w/2-0.5, 0,
+                                      0, 726.0941816535367, h/2-0.5, 0,
                                       0, 0,                 1,     0]);
     this.setValue(dist, projection);
   }
